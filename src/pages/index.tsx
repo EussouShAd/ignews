ec833b50c3eebb9b1fada515a366e3b7e7e964ec
Luciano Campos <eussoushad@gmail.com>
2022-11-17 12:05:55 -0300

    Static Site Generatrion SSG

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,4 +1,4 @@
-import { GetServerSideProps } from 'next';
+import { GetStaticProps } from 'next';
 import Head from 'next/head';
 import { SubscribeButton } from '../components/subscribeButton';
 import { stripe } from '../services/stripe';
@@ -35,7 +35,7 @@ export default function Home({ product }: HomeProps) {
   )
 }
 
-export const getServerSideProps: GetServerSideProps = async () => {
+export const getStaticProps: GetStaticProps = async () => {
   const price = await stripe.prices.retrieve('price_1M56T4FkVOmmchPl8t3PmGPl')
   const product = {
     priceId: price.id,
@@ -47,6 +47,7 @@ export const getServerSideProps: GetServerSideProps = async () => {
   return { 
     props: {
       product,
-    }
+    },
+    revalidate: 60 * 60 * 24, // 24 hours
   }
 }
